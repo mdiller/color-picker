f6522a7bf69ad5975c2081c5c973ec1cf95d67f4
Malcolm Diller <malcolm@dillerm.io>
2022-11-01 20:39:31 -0700

    updated to kindof support hsv

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,65 @@
 		</div>
 		<gradient-slider
 			v-model:value="color.r"
-			color1="rgba(0, var(--color-g), var(--color-b), var(--slider-opacity))"
+			color1="rgba(  0, var(--color-g), var(--color-b), var(--slider-opacity))"
 			color2="rgba(255, var(--color-g), var(--color-b), var(--slider-opacity))"
 			:min="0"
 			:max="255"
 		/>
 		<gradient-slider
 			v-model:value="color.g"
-			color1="rgba(var(--color-r), 0, var(--color-b), var(--slider-opacity))"
+			color1="rgba(var(--color-r),   0, var(--color-b), var(--slider-opacity))"
 			color2="rgba(var(--color-r), 255, var(--color-b), var(--slider-opacity))"
 			:min="0"
 			:max="255"
 		/>
 		<gradient-slider
 			v-model:value="color.b"
-			color1="rgba(var(--color-r), var(--color-g), 0, var(--slider-opacity))"
+			color1="rgba(var(--color-r), var(--color-g),   0, var(--slider-opacity))"
 			color2="rgba(var(--color-r), var(--color-g), 255, var(--slider-opacity))"
 			:min="0"
 			:max="255"
+		/>
+		<br/>
+		<gradient-slider
+			v-model:value="color.h"
+			color1="hsla(0, var(--color-s), var(--color-v), var(--slider-opacity))"
+			color2="hsla(356, var(--color-s), var(--color-v), var(--slider-opacity))"
+			:min="0"
+			:max="356"
+		/>
+		<gradient-slider
+			v-model:value="color.s"
+			color1="hsla(var(--color-h),   0%, var(--color-v), var(--slider-opacity))"
+			color2="hsla(var(--color-h), 100%, var(--color-v), var(--slider-opacity))"
+			:is_percent="true"
+			:min="0"
+			:max="100"
+		/>
+		<gradient-slider
+			v-model:value="color.v"
+			color1="hsla(var(--color-h), var(--color-s), 0%, var(--slider-opacity))"
+			color2="hsla(var(--color-h), var(--color-s), 100%, var(--slider-opacity))"
+			:is_percent="true"
+			:min="0"
+			:max="100"
 		/>
 	</div>
 </template>
 
 <script>
 import DillermNavBar from "@dillerm/webutils/src/components/DillermNavBar.vue"
-import { rgbToHex } from "@dillerm/webutils/src/utils.js"
+import { rgbToHex, rgbToHsv, hsvToRgb } from "@dillerm/webutils/src/utils.js"
+
 
 import GradientSlider from "./components/GradientSlider.vue";
 var COLOR_PROP_MAP = {
 	r: "--color-r",
 	g: "--color-g",
 	b: "--color-b",
+	h: "--color-h",
+	s: "--color-s",
+	v: "--color-v",
 	hex: "--color-hex"
 }
 
@@ -57,6 +85,9 @@ export default {
 				r: 55,
 				g: 44,
 				b: 22,
+				h: 100,
+				s: 50,
+				v: 20,
 				hex: ""
 			},
 			shown_color: {},
@@ -69,8 +100,16 @@ export default {
 			Object.keys(COLOR_PROP_MAP).forEach(prop => {
 				if (force_update || (this.shown_color[prop] != this.color[prop])) {
 					if (this.$el && this.$el.parentElement) { // the immediate calls this before mounting, so 
-						this.shown_color[prop] = this.color[prop];
-						this.$el.parentElement.style.setProperty(COLOR_PROP_MAP[prop], this.color[prop]);
+						var color = this.color[prop]
+						this.shown_color[prop] = color;
+						if ((typeof color) == "number") {
+							color = color.toFixed();
+						}
+						if (["s", "v"].includes(prop)) {
+							color = `${color}%`;
+						}
+						// console.log("setting", COLOR_PROP_MAP[prop], color)
+						this.$el.parentElement.style.setProperty(COLOR_PROP_MAP[prop], color);
 					}
 				}
 			});
@@ -84,14 +123,28 @@ export default {
 			else {
 				changed_keys = Object.keys(COLOR_PROP_MAP).filter(prop => this.color[prop] != this.shown_color[prop])
 			}
-			
-			this.color.hex = rgbToHex(this.color);
+			if (changed_keys.length > 0) {
+				console.log("reCalculateColors()", changed_keys)
+				if (changed_keys.filter(key => [ "r", "g", "b" ].includes(key)).length > 0) {
+					Object.assign(this.color, rgbToHsv(this.color));
+				}
+				else {
+					Object.assign(this.color, hsvToRgb(this.color));
+				}
+
+				this.color.hex = rgbToHex({
+					r: this.color.r.toFixed(),
+					g: this.color.g.toFixed(),
+					b: this.color.b.toFixed()
+				});
+			}
 		}
 	},
 	watch: {
 		color: {
 			handler() {
 				if (this.handling_change) {
+					console.log("skipping")
 					return;
 				}
 				this.handling_change = true;
@@ -99,12 +152,14 @@ export default {
 				this.updateColorProps();
 				this.handling_change = false;
 			},
-			deep: true,
-			immediate: true
+			deep: true
 		}
 	},
 	mounted() {
+		this.handling_change = true;
+		this.reCalculateColors();
 		this.updateColorProps(true);
+		this.handling_change = false;
 	}
 };
 
